d76a943e8e5bef6803d2e03eea7ef94b1ee3c21d
Henrik Hodne <henrik@travis-ci.com>
2016-03-11 15:20:44 +0100

    cloud: fix tests

--- cloud/fake_test.go
+++ cloud/fake_test.go
@@ -8,22 +8,25 @@ var _ Provider = &FakeProvider{}
 func TestFakeProviderCreate(t *testing.T) {
 	provider := &FakeProvider{}
 
-	_, err := provider.Create(CreateAttributes{ImageName: ""})
+	_, err := provider.Create("no-image-name", CreateAttributes{ImageName: ""})
 	if err == nil {
 		t.Errorf("expected error, got nil")
 	}
 
-	_, err = provider.Create(CreateAttributes{ImageName: "nonexistant-image"})
+	_, err = provider.Create("invalid-image-name", CreateAttributes{ImageName: "nonexistant-image"})
 	if err == nil {
 		t.Errorf("expected error, got nil")
 	}
 
-	instance, err := provider.Create(CreateAttributes{ImageName: "standard-image"})
+	instance, err := provider.Create("valid-image-name", CreateAttributes{ImageName: "standard-image"})
 	if err != nil {
 		t.Errorf("provider.Create returned error: %v", err)
 	}
 
 	if instance.State != InstanceStateStarting {
 		t.Errorf("expected state to be %v, was %v", InstanceStateStarting, instance.State)
+	}
+	if instance.ID != "valid-image-name" {
+		t.Errorf("expected ID to be %v, was %v", "valid-image-name", instance.ID)
 	}
 }
